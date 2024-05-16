e9d03408230a6ecd9c1a229e9bbebdfedb176b1a
Дмитрий <dbilica19@gmail.com>
2024-05-16 05:03:15 +0300

    сделал два теста для User Service P.S. сломал всю голову

--- MyWebApi.Business.Tests/Services/UserServiceTest.cs
+++ MyWebApi.Business.Tests/Services/UserServiceTest.cs
@@ -1,7 +1,8 @@
 ﻿using AutoMapper;
 using Moq;
-using MyWebApi.Business.IServices;
+using MyWebApi.Business.Models;
 using MyWebApi.Business.Models.Request;
+using MyWebApi.Business.Models.Responses;
 using MyWebApi.Business.Services;
 using MyWebApi.Core.Dtos;
 using MyWebApi.DataLayer.IRepository;
@@ -10,20 +11,22 @@ namespace MyWebApi.Business.Tests.Services
 {
     public class UserServiceTest
     {
-        private const string _guidPattern = "^[{(]?[0-9A-F]{8}[-]?(?:[0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$)";
         private readonly Mock<IUsersRepository> _usersRepositoryMock;
         private readonly IMapper _mapper;
-        private IUserServices _sut;
         public UserServiceTest()
         {
-            _usersRepositoryMock = new Mock<IUsersRepository>();
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new UsersMappingProfile());
+            });
 
+            _mapper = new Mapper(config);
+            _usersRepositoryMock = new Mock<IUsersRepository>();
         }
 
         [Fact]
-        public void AddUserTest()
+        public void AddUserTest_ValidRequestGuid()
         {
-
             var validUserDto = new CreateUserRequest()
             {
                 UserName = "Name",
@@ -32,14 +35,36 @@ namespace MyWebApi.Business.Tests.Services
                 Age = 23
             };
             var expectedGuid = Guid.NewGuid();
+
             _usersRepositoryMock.Setup(x => x.AddUser(It.IsAny<UserDto>())).Returns(expectedGuid);
+
             var sut = new UserServices(_usersRepositoryMock.Object, _mapper);
 
             var actual = sut.AddUser(validUserDto);
 
-            Assert.Matches(_guidPattern, actual.ToString());
-            Assert.NotEqual("00000000-0000-0000-0000-000000000000", actual.ToString());
+            Assert.Equal(expectedGuid, actual);
+            _usersRepositoryMock.Verify(r => r.AddUser(It.IsAny<UserDto>()), Times.Once);
+        }
 
+        [Fact]
+        public void AllUsersTest_ReturnListUsers()
+        {
+            var expectedList = new List<User>() 
+            { new() {Email = "user1@mail.ru" }, 
+            { new() {Email = "user2@mail.ru" }}};
+
+            var expectedListDto = new List<UserDto>() 
+            { new() { Email = "user1@mail.ru" }, 
+            { new() { Email = "user2@mail.ru" }}};
+
+            _usersRepositoryMock.Setup(u => u.GetUsers()).Returns(expectedListDto);
+
+            var sut = new UserServices(_usersRepositoryMock.Object, _mapper);
+
+            var actual = sut.GetUsers();
+
+            actual.Equals(expectedList);
+            _usersRepositoryMock.Verify(r => r.GetUsers(), Times.Once);
         }
     }
 }
